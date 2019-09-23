1f2cb5cb024f11b6380144703433b5c1c26e13e0
HonmaMeiko <17mwang2@stu.edu.cn>
2019-09-23 21:55:24 +0800

    添加数据库部署环境

---
+++ config/config.prod.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  sequelize: {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    database: 'info_integration',
+    username: 'root',
+    password: '1234',
+    timezone: '+08:00',
+  },
+}
