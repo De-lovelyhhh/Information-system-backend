f854f14783b2462923c262ee98a626c16fe2d3a4
HonmaMeiko <17mwang2@stu.edu.cn>
2019-09-28 12:18:37 +0800

    '修复数据库端口bug'

--- config/config.local.js
+++ config/config.local.js
@@ -4,7 +4,7 @@ module.exports = {
   sequelize: {
     dialect: 'mysql',
     host: 'cdb-ljkh0213.gz.tencentcdb.com',
-    port: 3306,
+    port: 10129,
     database: 'information-backend_dev',
     username: 'root',
     password: 'ybalabala0424',
